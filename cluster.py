corrected hist calculation

cluster.py
@@ -4,7 +4,7 @@ Cluster pixels of an image and create corresponding masks.
 **********************************************************
 """
 # coding: utf8
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from data import get_generators
 from histogram import hist_cv
 import numpy as np
@@ -21,7 +21,7 @@ def get_centroids(itemlist, extractor, save_path, n_clusters=8, batch_size=16, p
     """
     gen, _ = get_generators(itemlist, batch_size, 1., patch_size, 1.)
     fts = extractor.predict_generator(gen, steps=len(gen))
-    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans = MiniBatchKMeans(n_clusters=n_clusters)
     kmeans.fit(fts)
     dump(kmeans, save_path)
     return kmeans
@@ -43,5 +43,7 @@ def get_histograms(itemlist, extractor, kmeans, save_path, batch_size=16, patch_
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         for i in range(n_clusters):
             hist[i] += hist_cv(img, mask=(mask == i))
+    hist = hist.reshape(256, -1).cumsum(0)
+    hist /= hist.max(0)
     np.save(save_path, hist)
     return hist

histogram.py
@@ -20,10 +20,10 @@ def get_lut(h_src, h_ref):
 
     ********************************************************************
     """
-    h_src = h_src.cumsum(axis=0)
-    h_src /= h_src.max()
-    h_ref = h_ref.cumsum(axis=0)
-    h_ref /= h_ref.max()
+    h_src = h_src.cumsum(0)
+    h_src /= h_src.max(0)
+    h_ref = h_ref.cumsum(0)
+    h_ref /= h_ref.max(0)
 
     lut = np.argmax((h_ref[:, None] >= h_src[None]), axis=0)
     return lut